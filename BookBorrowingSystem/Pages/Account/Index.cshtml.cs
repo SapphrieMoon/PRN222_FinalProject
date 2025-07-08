4302bc58afb3a8157f6fe8a01bcc88b7e4d15812
SapphrieMoon <caidenlam@gmail.com>
2025-07-08 19:03:34 +0700

    feat: logout

---
+++ BookBorrowingSystem/Pages/Account/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookBorrowingSystem.Pages.Account
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
