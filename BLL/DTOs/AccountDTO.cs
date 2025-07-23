8a6b9e8a97134d3be28debbcefd26c90ea6f894d
SapphrieMoon <caidenlam@gmail.com>
2025-07-24 00:14:53 +0700

    update login ui

--- BLL/DTOs/AccountDTO.cs
+++ BLL/DTOs/AccountDTO.cs
@@ -23,4 +23,19 @@ namespace BLL.DTOs
         public string Role { get; set; }
         public int AccountId { get; set; }
     }
+
+    public class RegisterDTO
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
 }
---
+++ DAL/Migrations/20250723165951_UpdateAccountModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAccountModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Accounts",
+                keyColumn: "AccountId",
+                keyValue: 3,
+                column: "Role",
+                value: "User");
+
+            migrationBuilder.UpdateData(
+                table: "BorrowRequests",
+                keyColumn: "RequestId",
+                keyValue: 1,
+                columns: new[] { "BorrowDate", "RequestDate", "ReturnDate" },
+                values: new object[] { new DateTime(2025, 7, 24, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2025, 7, 23, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2025, 7, 30, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Accounts",
+                keyColumn: "AccountId",
+                keyValue: 3,
+                column: "Role",
+                value: "Student");
+
+            migrationBuilder.UpdateData(
+                table: "BorrowRequests",
+                keyColumn: "RequestId",
+                keyValue: 1,
+                columns: new[] { "BorrowDate", "RequestDate", "ReturnDate" },
+                values: new object[] { new DateTime(2025, 7, 18, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2025, 7, 17, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2025, 7, 24, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+    }
+}
