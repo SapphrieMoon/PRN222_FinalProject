0409e8eada2613949fbde956f03cfb728013806b
AntiHero2311 <hungntse184131@fpt.edu.vn>
2025-07-28 19:38:39 +0700

    update signalR

---
+++ BookBorrowingSystem/Pages/Hubs/LibraryHub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BookBorrowingSystem.Pages.Hubs
+{
+    public class LibraryHub : Hub
+    {
+        public async Task NotifyBorrow(string bookTitle)
+        {
+            await Clients.All.SendAsync("BookBorrowed", bookTitle);
+        }
+
+        public async Task NotifyReturn(string bookTitle)
+        {
+            await Clients.All.SendAsync("BookReturned", bookTitle);
+        }
+
+        // Thêm method mới để thông báo khi có sách mới
+        public async Task NotifyBookAdded(string bookTitle, string author)
+        {
+            await Clients.All.SendAsync("BookAdded", bookTitle, author);
+        }
+
+        // Thêm method để refresh toàn bộ danh sách sách
+        public async Task RefreshBookList()
+        {
+            await Clients.All.SendAsync("RefreshBooks");
+        }
+    }
+}
--- BookBorrowingSystem/Pages/ManageBook/Create.cshtml.cs
+++ BookBorrowingSystem/Pages/ManageBook/Create.cshtml.cs
@@ -3,6 +3,8 @@ using BLL.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.SignalR;
+using BookBorrowingSystem.Pages.Hubs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +16,12 @@ namespace BookBorrowingSystem.Pages.ManageBook
     public class CreateModel : PageModel
     {
         private readonly IBookService _bookService;
+        private readonly IHubContext<LibraryHub> _hubContext;
 
-        public CreateModel(IBookService bookService)
+        public CreateModel(IBookService bookService, IHubContext<LibraryHub> hubContext)
         {
             _bookService = bookService;
+            _hubContext = hubContext;
         }
 
         [BindProperty]
@@ -38,6 +42,12 @@ namespace BookBorrowingSystem.Pages.ManageBook
             try
             {
                 _bookService.AddBook(Book);
+
+                await _hubContext.Clients.All.SendAsync("ReloadBookIndex");
+                // Gửi thông báo real-time khi thêm sách thành công
+                //await _hubContext.Clients.All.SendAsync("BookAdded", Book.Title, Book.Author);
+                //TempData["SuccessMessage"] = $"Book '{Book.Title}' has been added successfully!";
+
                 return RedirectToPage("./Index");
             }
             catch (Exception ex)
