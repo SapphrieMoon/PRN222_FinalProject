52fa2200c228db3eb71b5d99a19593f4a93c1bd1
AnPTSE183963 <AnPTSE183963@fpt.edu.vn>
2025-07-29 20:40:20 +0700

    Update History.cshtml.cs

--- BookBorrowingSystem/Pages/Request/History.cshtml.cs
+++ BookBorrowingSystem/Pages/Request/History.cshtml.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 
 namespace BookBorrowingSystem.Pages.Request
 {
-    [Authorize(Roles = "Librarian")]
+    [Authorize()]
     public class HistoryModel : PageModel
     {
         private readonly IRequestService _requestService;
